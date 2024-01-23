6a3521309402e4d281cc1b7571b3be4745716e3a
Taras <topanasenko85@gmail.com>
2024-01-23 15:53:23 +0200

    start redux

---
+++ src/store/store.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux'
+
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
+
+const reducer = (state) => {
+    
+    return state
+}
+
+const store = createStore(reducer, {
+    filter: '',
+    contacts:[]
+})
+
+
+console.log('Store:', store)
+
+store.dispatch({ type: 'contacts', payload: [2] })
+
+console.log(store.getState());
