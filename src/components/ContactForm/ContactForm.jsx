757fb966eae336707859b8ba5e6baa5cdb65bad7
Taras <topanasenko85@gmail.com>
2024-01-24 00:05:30 +0200

    redux and mask

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 // import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
+import InputMask from 'react-input-mask';
 
 const ContactForm = ({ addContact }) => {
-
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
@@ -25,40 +25,50 @@ const ContactForm = ({ addContact }) => {
     setName('');
     setNumber('');
   };
-    return (
+  return (
+    <form className={s.containerForms} onSubmit={handleSubmit}>
+      <input
+        placeholder="Name..."
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
 
-        <form className={s.containerForms} onSubmit={handleSubmit} >
-          <input
-            
-            placeholder="Name..."
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-        />
-          <input
-            
-            placeholder="Number..."
-            type="tel"
-            name="number"
-            value={number}
-            onChange={handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-        />
 
-          <button type="submit">
-            Add contact
-          </button>
-        </form>
+      <InputMask
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
 
-    );
-  }
+        mask="+38(099)999-99-99" // Задайте свою маску телефонного номера
+        maskChar="_" // Задайте символ заповнювача (необов'язково)
+        // placeholder="+38 (___) ___-__-__"
+        placeholder="Number..."
+        
+      />
+      {/* <input
+        placeholder="Number..."
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      /> */}
 
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
 
 ContactForm.propTypes = {
   addContact: PropTypes.func.isRequired,
